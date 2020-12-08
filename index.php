3d85b4a1c23fa0fc8a3cbf8c0c76cd8c77205959
KubaNocon7890 <42960635+KonneckY@users.noreply.github.com>
2020-12-08 12:04:55 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -12,11 +12,23 @@
         <h1 class="title">EGZAMIN EE.09</h1>
     </header>
     <main>
-        <p class="pytanie">5. Edytując grafikę w edytorze grafiki rastrowej należy pozbyć się kolorów z rysunku tak, aby obraz był w odcieniach szarości. Można do tego efektu wykorzystać funkcję</p>
+        <p class="pytanie">Edytując grafikę w edytorze grafiki rastrowej należy pozbyć się kolorów z rysunku tak, aby obraz był w odcieniach szarości. Można do tego efektu wykorzystać funkcję</p>
         <p class="odpowiedz">A. Kadrowania</p>
         <p class="odpowiedzD">B. Desaturacji</p>
         <p class="odpowiedz">C. szumu RGB</p>
         <p class="odpowiedz">D. filtru rozmycia</p>
+        <hr>
+        <p class="pytanie">W MS SQL Server predefiniowana rola o nazwie dbcreator pozwala użytkownikowi na</p>
+        <p class="odpowiedz">A. zarządzanie plikami na dysku</p>
+        <p class="odpowiedzD">B. tworzenie, modyfikowanie, usuwanie i odzyskiwanie bazy danych</p>
+        <p class="odpowiedz">C. wykonywanie każdej operacji na serwerze i posiadanie prawa własności każdej bazy</p>
+        <p class="odpowiedz">D. zarządzanie bezpieczeństwem systemu</p>
+        <hr>
+        <p class="pytanie">Normalizacja tabel jest procesem, który ma na celu</p>
+        <p class="odpowiedz">A. przedstawienie graficzne bazy</p>
+        <p class="odpowiedz">B. jedynie utworzenie tabel i relacji w bazie</p>
+        <p class="odpowiedzD">C. sprawdzenie i optymalizację bazy danych</p>
+        <p class="odpowiedz">D. dodanie rekordów do bazy</p>
         <hr>
         
     </main>
